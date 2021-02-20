310afb020332d72b04cdf2143036b55954c38b3c
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-02-20 16:21:41 +0330

    s

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends React.Component
 {
     render()
     {
+        let {toggle} = this.props.state;
         return(
             <Fragment>
-                <AppInfo />
-                <Calculator />
+                {toggle ? <Calculator /> : <AppInfo />}
             </Fragment>
         );
     }
