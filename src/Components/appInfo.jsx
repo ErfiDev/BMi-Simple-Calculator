c4d3eb17b3e8241052c81b467032fa64bf498a38
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-02-20 12:18:58 +0330

    first component style

--- src/Components/appInfo.jsx
+++ src/Components/appInfo.jsx
@@ -4,7 +4,18 @@ import React from 'react';
 const AppInfo = ()=>{
     return(
         <div className="info-container">
+            <h1>
+                bmi simple calculator <br />
 
+                <span
+                class="txt-type"
+                data-wait="3000"
+                data-words='["Made by React js and Redux" , "Prepared by Erfan Hanifezade"]'
+                ></span>
+            </h1>
+
+
+            <button className="start-btn">I'm Ready</button>
         </div>
     );
 }
