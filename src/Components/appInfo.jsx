ba68c5dcc3d29f156ec96765c0c1977d0ce2ab37
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-02-21 15:28:35 +0330

    fix a bug

--- src/Components/appInfo.jsx
+++ src/Components/appInfo.jsx
@@ -14,7 +14,7 @@ const AppInfo = ()=>{
 
                 <span
                 class="txt-type"
-                data-wait="3000"
+                data-wait="1000"
                 data-words='["Made by React js and Redux" , "Prepared by Erfan Hanifezade"]'
                 ></span>
             </h1>
